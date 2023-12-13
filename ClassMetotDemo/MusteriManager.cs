c96c4444850e2794a95af1ff5e67c4712dd279d0
umut_ <umut_@DESKTOP-4MRUON5>
2023-12-13 11:36:15 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class MusteriManager
+    {
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi " + musteri.FirstName + " " + musteri.LastName);
+        }
+        
+        public void Remove (Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Silidi " + musteri.FirstName + " " + musteri.LastName);
+        }
+
+        public void Update(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Güncellendi " + musteri.FirstName + " " + musteri.LastName);
+        }
+
+        public void List(Musteri[] musteri)
+        {
+            Console.WriteLine("Müşteriler Listelendi ");
+            foreach (Musteri m in musteri) 
+            { 
+                Console.WriteLine(m.FirstName + " " + m.LastName); 
+            }
+            
+        }
+
+        
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ClassMetotDemo;
+
+Musteri musteri1 = new Musteri()
+{
+    Id = 1,
+    FirstName = "Umut",
+    LastName="Bulundu",
+    Number="0484156489",
+};
+Musteri musteri2 = new Musteri()
+{
+    Id= 2,
+    FirstName = "Engin",
+    LastName = "Demirog",
+    Number = "03586570876",
+};
+Musteri[] musteris = new Musteri[] { musteri1, musteri2 };
+MusteriManager musteriManager = new MusteriManager();
+musteriManager.Add(musteri1 as Musteri);
+musteriManager.Add(musteri2 as Musteri);
+Console.WriteLine("-------------");
+musteriManager.List(musteris);
+Console.WriteLine("------------");
+musteriManager.Remove(musteri1);
+musteriManager.Remove(musteri2);
+Console.WriteLine("-------------");
+musteriManager.List(musteris);
+
+
+
